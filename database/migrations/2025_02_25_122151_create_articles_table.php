d779351a286cd44719aa0b885671a097cf025251
Nico Deblauwe <nico@bluepundit.eu>
2025-02-25 20:19:57 +0700

    REFACTOR :hammer: Use User model for the author information

--- database/migrations/2025_02_25_122151_create_articles_table.php
+++ database/migrations/2025_02_25_122151_create_articles_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
 
             $table->string('title');
             $table->text('content')->nullable();
-            $table->string('author_name')->nullable();
+            $table->foreignId('author_id');
 
             $table->timestamps();
         });
