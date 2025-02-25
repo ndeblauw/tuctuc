ef84c4a24c219007171c27f44dce8a4985703432
Nico Deblauwe <nico@bluepundit.eu>
2025-02-25 20:12:39 +0700

    FEATURE ✨ Create Comment model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Article;
+use App\Models\Comment;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Article::factory(10)->create();
+        Comment::factory(25)->create();
         User::factory(10)->create();
     }
 }
