2243d600b245f115087571ec1334f6cdef400c95
Nico Deblauwe <nico@bluepundit.eu>
2025-02-26 19:26:17 +0700

    DESIGN :art: Layout article index and show with tailwind

--- resources/views/site/articles/index.blade.php
+++ resources/views/site/articles/index.blade.php
@@ -1,9 +1,12 @@
-<h2>Articles</h2>
 <x-site-layout>
+
+    <h2 class="font-bold text-2xl">Articles</h2>
+
+    <ul class="list-disc pl-4">
+        @foreach($articles as $article)
+            <li><a class="underline" href="/articles/{{$article->id}}">{{$article->title}}</a></li>
+        @endforeach
+    </ul>
+
 </x-site-layout>
 
-<ul>
-    @foreach($articles as $article)
-        <li><a href="/articles/{{$article->id}}">{{$article->title}}</a></li>
-    @endforeach
-</ul>
--- resources/views/site/articles/show.blade.php
+++ resources/views/site/articles/show.blade.php
@@ -1,8 +1,7 @@
-<h2>{{$article->title}}</h2>
 <x-site-layout>
+    <h2 class="font-bold mb-4 text-4xl">{{$article->title}}</h2>
 
-<p>
-    {{$article->content}}
-
-</p>
+    <p>
+        {{$article->content}}
+    </p>
 </x-site-layout>
