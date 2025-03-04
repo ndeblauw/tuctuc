8a01b13bc551cb97dd103c1b1806fcf8263c8cf7
Nico Deblauwe <nico@bluepundit.eu>
2025-03-04 13:30:46 +0700

    DESIGN :art: Improve the admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,10 +1,18 @@
 <x-site-layout>
-    This is the admin dashboard
-
-    Your name is {{auth()->user()->name}}
 
     <div>
-        <h2>Your articles</h2>
+        This is the admin dashboard
+
+        Your name is {{auth()->user()->name}}
+    </div>
+
+    <div>
+        <div class="flex justify-between">
+            <h2 class="font-bold text-2xl">Your articles</h2>
+
+            <a href="{{route('admin.articles.create')}}" class="bg-purple-100 text-purple-500 hover:bg-purple-200 p-2 rounded uppercase font-semibold">Create article</a>
+        </div>
+
         <ul class="list-disc pl-4">
             @foreach(auth()->user()->articles as $article)
                 <li>{{$article->title}}</li>
