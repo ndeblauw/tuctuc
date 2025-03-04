993833a831e782f198301261765690a7555e244e
Nico Deblauwe <nico@bluepundit.eu>
2025-03-04 22:48:46 +0700

    DESIGN :art: Some small cosmetic changes

--- resources/views/components/site-layout-header.blade.php
+++ resources/views/components/site-layout-header.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-purple-700 text-purple-50">
     <div class="max-w-7xl m-auto flex justify-between items-center h-10">
-        <div class="font-bold text-lg">TucTuc</div>
+        <a href="{{route('home')}}" class="font-bold text-lg">TucTuc</a>
         <ul class="flex gap-x-4">
             @foreach($menu as $label => $url)
                 <li><a class="hover:font-semibold hover:text-white" href="{{$url}}">{{$label}}</a></li>
--- resources/views/site/articles/show.blade.php
+++ resources/views/site/articles/show.blade.php
@@ -1,5 +1,8 @@
 <x-site-layout>
 
+    @foreach($article->keywords as $keyword)
+        <span class="bg-yellow-200 text-yellow-600 p-1">{{$keyword->title}}</span>
+    @endforeach
     <h2 class="font-bold mb-4 text-4xl">{{$article->title}}</h2>
 
 
