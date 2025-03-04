8ca7ad7d65e55c59adac740f4ddd194ae85c55fb
Nico Deblauwe <nico@bluepundit.eu>
2025-03-04 22:49:33 +0700

    FEATUREE :sparkles: Improved data loading in welcome page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,8 +10,10 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $articles = Article::take(4)->latest()->get();
-        $authors = User::whereHas('articles')->orderBy('name')->get();
+        $articles = Article::with('keywords', 'author')->take(4)->latest()->get();
+        $authors = User::withCount('articles')->whereHas('articles')->get();
+
+        $authors = $authors->sortByDesc( fn($author) => $author->articles_count )->take(4);
 
         return view('welcome', compact('articles', 'authors'));
     }
