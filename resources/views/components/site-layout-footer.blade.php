858c8717c8f2042b7925f6d9f1a8b3eb8b63375d
Nico Deblauwe <nico@bluepundit.eu>
2025-03-05 16:32:26 +0700

    DESIGN :art: Have the footer match the header

--- resources/views/components/site-layout-footer.blade.php
+++ resources/views/components/site-layout-footer.blade.php
@@ -1,5 +1,28 @@
-<div class="bg-purple-700 text-purple-50">
-    <div class="max-w-7xl m-auto">
-        <div>Our great footer</div>
+<div class="relative bg-purple-700 text-purple-50 border-t-6 border-purple-500 min-h-64">
+
+
+    <div class="absolute inset-0 bg-[url('/img/green-dust-and-scratches.png')] opacity-90 mix-blend-overlay z-0"></div>
+
+    <div class="max-w-7xl m-auto mt-8">
+        <div class="pt-16 grid grid-cols-6">
+            <div class="flex gap-4 items-baseline col-span-2">
+                <svg class="opacity-80  w-16 h-16 text-purple-50" xmlns="http://www.w3.org/2000/svg" class="w-12 h-12" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-rocket"><path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z"/><path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z"/><path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0"/><path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5"/></svg>
+                <div>
+                    <div class="font-semibold text-3xl">TucTuc</div>
+                    <div>Harbour.Space UTCC campus blog</div>
+                </div>
+            </div>
+            <div class="col-span-3">
+                Space for some menu options
+            </div>
+            <div>
+                <h2 class="font-bold mb-4">About us</h2>
+                <ul class="list-caret">
+                    <li>Contact</li>
+                    <li>Legal</li>
+                </ul>
+            </div>
+
+        </div>
     </div>
 </div>
