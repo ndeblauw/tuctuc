9a38635a891f61b05572d2d09b99edc51c84fd9f
Nico Deblauwe <nico@bluepundit.eu>
2025-03-05 17:45:16 +0700

    DESIGN :art: New spacy layout (#1)
    * DESIGN :sparkles: Apply texture to header background
    
    * DESIGN :art: Rework the logo area
    
    * DESIGN :art: Give authors and keywords own boxes
    
    * DESIGN :art: Box things up
    
    * CONTENT :pencil: Add quote on HS@UTCC
    
    * DESIGN :art: Style boxes with background icon
    
    * DESIGN :art: Have the footer match the header
    
    * DESIGN :art: Add HS alike background to design elements
    
    * BUGFIX :bug: Remove unneeded file

--- resources/views/components/site-layout-header.blade.php
+++ resources/views/components/site-layout-header.blade.php
@@ -1,6 +1,18 @@
-<div class="bg-purple-700 text-purple-50">
-    <div class="max-w-7xl m-auto flex justify-between items-center h-10">
-        <a href="{{route('home')}}" class="font-bold text-lg">TucTuc</a>
+<div class="relative bg-purple-700 text-purple-50 border-t-6 border-purple-500">
+    <div class="absolute inset-0 bg-[url('/img/gplay.png')] opacity-90 mix-blend-overlay z-0"></div>
+
+    <div class="relative max-w-7xl m-auto flex justify-between items-center h-20 z-50">
+        <div class="">
+            <div class="flex justify-start items-center gap-x-2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-rocket"><path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z"/><path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z"/><path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0"/><path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5"/></svg>
+                <div class="flex flex-col">
+                    <a href="{{route('home')}}" class="font-bold text-lg">TucTuc</a>
+                    <span class="text-xs italic">Harbour.Space UTCC campus blog</span>
+                </div>
+            </div>
+
+
+        </div>
         <ul class="flex gap-x-4">
             @foreach($menu as $label => $url)
                 <li><a class="hover:font-semibold hover:text-white" href="{{$url}}">{{$label}}</a></li>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,13 +10,44 @@
 
         </div>
 
-        <div class="bg-purple-50 p-4">
-            <h3 class="font-bold">Our top authors</h3>
-            @foreach($authors as $author)
-                {{$author->name}}<br/>
-            @endforeach
+        <div>
+            <div class="relative bg-purple-50 rounded-xl mb-8 p-4 overflow-hidden min-h-32 border-2 border-purple-200">
+                <div class="absolute inset-0 bg-[url('/img/gplay.png')] opacity-40 mix-blend-darken "></div>
 
-            <h3 class="font-bold mt-12">Our top keywords</h3>
+                <svg class="absolute -bottom-6 -right-6 opacity-40  w-32 h-32 text-purple-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-rocket"><path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z"/><path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z"/><path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0"/><path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5"/></svg>
+
+                <h2 class="font-bold text-purple-800 mb-4">Harbour.Space @ UTCC</h2>
+
+                <p class="text-purple-950">
+                    Where innovation meets ambition, and student life is a journey of collaboration, creativity, and endless possibilities.
+                </p>
+            </div>
+
+            <div class="relative bg-slate-50 rounded-xl mb-8 p-4 overflow-hidden min-h-32 border-2 border-slate-200">
+                <div class="absolute inset-0 bg-[url('/img/gplay.png')] opacity-40 mix-blend-darken "></div>
+
+                <svg class="absolute -bottom-2 -right-6 opacity-40  w-32 h-32 text-slate-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-pen"><path d="M11.5 15H7a4 4 0 0 0-4 4v2"/><path d="M21.378 16.626a1 1 0 0 0-3.004-3.004l-4.01 4.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z"/><circle cx="10" cy="7" r="4"/></svg>
+
+                <h2 class="font-bold text-slate-800 mb-4">Our top contributors</h2>
+
+                <p class="text-black">
+                    @foreach($authors as $author)
+                        {{$author->name}}<br/>
+                    @endforeach
+                </p>
+            </div>
+            <div class="relative bg-yellow-50 rounded-xl mb-8 p-4 overflow-hidden min-h-32 border-2 border-yellow-200">
+                <div class="absolute inset-0 bg-[url('/img/gplay.png')] opacity-40 mix-blend-darken "></div>
+
+                <svg class="absolute -bottom-6 -right-6 opacity-40  w-32 h-32 text-yellow-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-tags"><path d="m15 5 6.3 6.3a2.4 2.4 0 0 1 0 3.4L17 19"/><path d="M9.586 5.586A2 2 0 0 0 8.172 5H3a1 1 0 0 0-1 1v5.172a2 2 0 0 0 .586 1.414L8.29 18.29a2.426 2.426 0 0 0 3.42 0l3.58-3.58a2.426 2.426 0 0 0 0-3.42z"/><circle cx="6.5" cy="9.5" r=".5" fill="currentColor"/></svg>
+
+                <h2 class="font-bold text-yellow-800 mb-4">Our top keywords</h2>
+
+                <p class="text-black">
+
+                </p>
+            </div>
+
         </div>
     </div>
 </x-site-layout>
