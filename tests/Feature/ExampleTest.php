dedf355be33e7386b5bf0f52bf1ab0819a1663f2
Nico Deblauwe <nico@bluepundit.eu>
2025-03-14 01:00:27 +0700

    BUGFIX :bug: Update the test for the external api usage

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,6 +1,10 @@
 <?php
 
+use Mockery\MockInterface;
+
 it('returns a successful response', function () {
+    Http::fake(['https://api.api-ninjas.com/v1/quotes' => Http::response(json_encode([['quote' => 'No quote available', 'author' => 'nobody', 'category' => 'empty']]), 200),]);
+
     $response = $this->get('/');
 
     $response->assertStatus(200);
