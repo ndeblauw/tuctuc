f3afebacfe4e1f94c3105e60166403a57cb690ce
Nico Deblauwe <nico@bluepundit.eu>
2025-03-14 00:21:00 +0700

    REFACTOR :hammer: also have starterkit using tailwind CDN

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -6,5 +6,6 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+<script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+
 @fluxAppearance
