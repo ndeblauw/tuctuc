cbe88ae83cebf0384f9b82a3f14fbbe591b7541f
Nico Deblauwe <nico@bluepundit.eu>
2025-03-14 00:17:05 +0700

    FEATURE :sparkles: Add API consumption (ninja api for quotes) to application

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Article;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
@@ -15,6 +16,27 @@ class HomeController extends Controller
 
         $authors = $authors->sortByDesc( fn($author) => $author->articles_count )->take(4);
 
-        return view('welcome', compact('articles', 'authors'));
+        $quote = cache()->remember(
+            'quote_for_welcome_page',
+            20,
+            fn() => $this->getQuote()
+        );
+
+        return view('welcome', compact('articles', 'authors', 'quote'));
+    }
+
+    private function getQuote()
+    {
+        $url = 'https://api.api-ninjas.com/v1/quotes';
+
+        try {
+            sleep(6);
+            $response = Http::withHeaders(['X-Api-Key' => env('APININJAKEY')])->get($url);
+            $quote = json_decode($response->body())[0];
+        } catch (\Exception) {
+            return (object) ['quote' => 'No quote available', 'author' => 'nobody', 'category' => 'empty'];
+        }
+
+        return $quote;
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,22 @@
         </div>
 
         <div>
+            <div class="relative bg-sky-50 rounded-xl mb-8 p-4 overflow-hidden min-h-32 border-2 border-sky-200">
+                <div class="absolute inset-0 bg-[url('/img/gplay.png')] opacity-40 mix-blend-darken "></div>
+
+                <svg class="absolute -bottom-2 -right-6 opacity-40  w-32 h-32 text-sky-200 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
+                </svg>
+
+
+                <h2 class="font-bold text-sky-800 mb-4">Inspirational quote</h2>
+
+                <p class="text-sky-950">
+                    <div class="font-semibold">{{$quote->quote}}</div>
+                    <div class="italic text-right">{{$quote->author}}</div>
+                </p>
+            </div>
+
             <div class="relative bg-purple-50 rounded-xl mb-8 p-4 overflow-hidden min-h-32 border-2 border-purple-200">
                 <div class="absolute inset-0 bg-[url('/img/gplay.png')] opacity-40 mix-blend-darken "></div>
 
